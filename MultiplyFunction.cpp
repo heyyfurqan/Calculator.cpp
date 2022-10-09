948eafa887796621befd8e9eea02dd4cf72201e4
Zain Hassan <81792650+zhassan9246@users.noreply.github.com>
2022-10-09 17:38:10 +0500

    Multiply
    m

---
+++ MultiplyFunction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+          cin>>h;
+          cin>>m;
+          cin>>s;
+          h=2;
+          m=45;
+          s=50;
+          int x=h*3600;
+          int y=m*6;
+          int z=s;
+          int t=x+y+z;
+return 0;
+}
